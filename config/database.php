b3418702b55afc87d92444939a3e72aac6183350
Your Name <913013106@qq.com>
2018-03-29 09:33:09 +0000

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -43,8 +43,8 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 'localhost'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
